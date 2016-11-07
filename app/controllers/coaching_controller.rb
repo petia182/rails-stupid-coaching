05e0bd279db8cd6fff63ddd0f11e03163a22c51b
Petya Nikolova <petia_182@yahoo.com>
2016-11-07 14:20:49 +0000

    frfr

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -7,13 +7,7 @@ class CoachingController < ApplicationController
   def coach_answer(your_message)
     if your_message.downcase == "i am going to work right now!"
       ''
-    elsif your_message.end_with?("?")
-      "Silly question, get dressed and go to work!"
-    else
-      "I don't care, get dressed and go to work!"
-    end
-  end
-
+    elsif your_message.end_with?("?")1
   def ask
   end
 
